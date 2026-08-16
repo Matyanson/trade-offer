4592ba093d2c3521c28314f480f107b7039697f8
Matyanson <matyas.perem@seznam.cz>
2026-08-16 21:34:44 +0200

    refactor(entry_order): cleanup replacing order

--- orders/entry_buy.py
+++ orders/entry_buy.py
@@ -1,38 +1,48 @@
 from orders.model.order_interface import OrderInterface, OrderState
 from alpaca.trading.enums import TimeInForce
 from alpaca.trading.models import OrderType
-from alpaca.trading.requests import LimitOrderRequest, StopLimitOrderRequest, MarketOrderRequest, StopOrderRequest
+from alpaca.trading.requests import LimitOrderRequest, OrderRequest, StopLimitOrderRequest, MarketOrderRequest, StopOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.data.requests import StockLatestQuoteRequest
 from setup.trading import trading_client
 from setup.market_data import crypto_client
 from alpaca.trading.models import Order
 
-def buy_at_price(symbol: str, qty: int, time_in_force: TimeInForce, entry_price: float) -> Order:
-    # Get current market price
+def get_order_type(entry_price: float, current_price: float) -> OrderType:
+    
+    # Price is already there -> buy immediately
+    if abs(current_price - entry_price) < 0.01:
+        return OrderType.MARKET
+    # We want to buy lower -> limit order
+    elif entry_price < current_price:
+        return OrderType.LIMIT
+    # We want to buy higher -> stop order
+    else:
+        return OrderType.STOP_LIMIT
+
+def get_current_price(symbol: str) -> float:
     quote = crypto_client.get_crypto_latest_quote(
         StockLatestQuoteRequest(symbol_or_symbols=symbol)
     )[symbol]
 
-    current_price = quote.ask_price
+    return float(quote.ask_price)
+
+def buy_at_price_request(symbol: str, qty: int, time_in_force: TimeInForce, entry_price: float) -> OrderRequest:
+
+    current_price = get_current_price(symbol)
 
     print(f"Current price: {current_price}")
     print(f"Target entry: {entry_price}")
 
-    # Price is already there -> buy immediately
-    if abs(current_price - entry_price) < 0.01:
-        order = trading_client.submit_order(
+    match get_order_type(entry_price, current_price):
+        case OrderType.MARKET:
             order_data=MarketOrderRequest(
                 symbol=symbol,
                 qty=qty,
                 side=OrderSide.BUY,
                 time_in_force=time_in_force,
             )
-        )
-
-    # We want to buy lower -> limit order
-    elif entry_price < current_price:
-        order = trading_client.submit_order(
+        case OrderType.LIMIT:
             order_data=LimitOrderRequest(
                 symbol=symbol,
                 qty=qty,
@@ -40,11 +50,7 @@ def buy_at_price(symbol: str, qty: int, time_in_force: TimeInForce, entry_price:
                 side=OrderSide.BUY,
                 time_in_force=time_in_force,
             )
-        )
-
-    # We want to buy higher -> stop order
-    else:
-        order = trading_client.submit_order(
+        case OrderType.STOP_LIMIT:
             order_data=StopLimitOrderRequest(
                 symbol=symbol,
                 qty=qty,
@@ -53,9 +59,8 @@ def buy_at_price(symbol: str, qty: int, time_in_force: TimeInForce, entry_price:
                 side=OrderSide.BUY,
                 time_in_force=TimeInForce.GTC,
             )
-        )
 
-    return order
+    return order_data
 
 
 class EntryBuy(OrderInterface):
@@ -67,41 +72,36 @@ class EntryBuy(OrderInterface):
         self.target_price = target_price
 
         # place an entry
-        alpaca_order = buy_at_price(
+        order_data = buy_at_price_request(
             symbol=symbol,
             qty=qty,
             time_in_force=TimeInForce.GTC,
             entry_price=target_price
         )
+        alpaca_order = trading_client.submit_order(order_data=order_data)
         
         super().__init__(symbol, qty, alpaca_order)
     
 
     def update(self):
-            """
-            Replace the order with a new one (updating the stop price, limit price, etc.)
-            """
-            match self.alpaca_order.type:
-                case OrderType.LIMIT:
-                    order_class = LimitOrderRequest
-                case OrderType.STOP_LIMIT:
-                    order_class = StopLimitOrderRequest
-                case OrderType.MARKET:
-                    order_class = MarketOrderRequest
-                case _:
-                    raise ValueError(f"Unsupported order type: {self.alpaca_order.type}")
+        current_price = get_current_price(self.symbol)
+        preferred_order_type = get_order_type(self.target_price, current_price)
 
-            # place a new order with updated parameters
+        order_data = buy_at_price_request(
+            symbol=self.symbol,
+            qty=abs(self.budget.budget_qty),
+            time_in_force=TimeInForce.GTC,
+            entry_price=self.target_price
+        )
+
+        # CANCEL: the target price is not in the same direction as the current order type, we need to cancel the order request
+        if self.alpaca_order.type != preferred_order_type:
+            trading_client.cancel_order_by_id(self.alpaca_order.id)
+            self.alpaca_order = trading_client.submit_order(order_data=order_data)
+        # REPLACE
+        else:
             self.alpaca_order = trading_client.replace_order_by_id(
                 order_id=self.alpaca_order.id,
-                order_data=order_class(
-                    symbol=self.symbol,
-                    qty=abs(self.budget.budget_qty),
-                    stop_price=self.stop_price,
-                    limit_price=self.limit_price,
-                    side=self.side,
-                    time_in_force=TimeInForce.GTC
-                )
+                order_data=order_data
             )
-
-            return self.alpaca_order.id
+        return self.alpaca_order.id
